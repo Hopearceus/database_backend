93dbdf1f1debbdcb65c4e971958cd0d4ea401455
Hopearceus <jxc2928@163.com>
2024-11-18 18:50:13 +0800

    [feat] moment urls补全

--- server/moment/urls.py
+++ server/moment/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 
 urlpattern = [
     path('creation', views.moment_creation, name='moment_creation'),
-    path()
+    path('deletion', views.moment_deletion, name='moment_deletion'),
+    path('detail', views.moment_detail, name='moment_detail'),
+    path('trip_share', views.trip_share, name='trip_share'),
+    path('comment', views.moment_comment, name='moment_comment'),
+    path('add_picture', views.moment_add_picture, name='moment_add_picture')
 ]
