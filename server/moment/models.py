93fb98c14b2a795bc4085fc95d6c3e5e65130384
Hopearceus <jxc2928@163.com>
2024-12-11 15:41:16 +0800

    [feat] moment 功能更新

--- server/moment/models.py
+++ server/moment/models.py
@@ -6,14 +6,15 @@ from django.db.models import CheckConstraint, Q, F
 
 class Moment(models.Model):
     mid = models.AutoField(primary_key=True)
-    creator = models.ForeignKey(Person, on_delete=models.CASCADE)
-    text = models.TextField()
-    tid = models.ForeignKey(Trip, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey('person.Person', on_delete=models.CASCADE)
+    content = models.TextField()
+    tid = models.ForeignKey('trip.Trip', on_delete=models.SET_NULL, null=True)
     discover = models.ImageField(upload_to='discover/', null=True)
     time = models.DateTimeField(auto_now_add=True)
+    aid = models.ForeignKey('album.Album', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.text[:50]
+        return self.content[:50]
     
 class Moment_Person(models.Model):
     pid = models.ForeignKey(Person, on_delete=models.CASCADE)
--- server/moment/views.py
+++ server/moment/views.py
@@ -3,20 +3,25 @@ import json
 
 from django.shortcuts import get_object_or_404, redirect
 from django.http import JsonResponse, HttpResponseForbidden
+from django.core.files.storage import FileSystemStorage
 from .models import Moment, Moment_Person
-from .forms import MomentForm
-from picture.models import Picture, Picture_Moment
+# from .forms import MomentForm
+from picture.models import Picture, Picture_Moment, Picture_Album
 from person.models import Person
+from album.models import Album
 from trip.models import Trip
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 import jwt
 
+import settings
+media_root = settings.MEDIA_ROOT
+base_url = settings.base_url
 SECRET_KEY = json.loads(open('../key.private').read())['SECRET_KEY']
 
 # @login_required
 def moment_add_picture(request, mid, pid):
-    pm = Picture_Moment(mid=mid, pid=pid)
+    pm = Picture_Moment.objects.create(mid=mid, pid=pid)
     pm.save()
     
     return JsonResponse({
@@ -40,9 +45,10 @@ def get_discover_moments(request):
         moments_data = [
             {
                 'mid': moment.mid,
-                'creator': moment.creator.username,
-                'text': moment.text,
-                'time': moment.time.isoformat(),
+                'username': moment.creator.username,
+                'userId': moment.creator.pid,
+                'content': moment.content,
+                'createTime': moment.time.strftime('%Y-%m-%d %H:%M:%S'),
                 'tid': moment.tid.tid if moment.tid else None,
             }
             for moment in moments
@@ -79,7 +85,7 @@ def add_comment(request):
         moment = get_object_or_404(Moment, mid=mid)
         username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
         person = get_object_or_404(Person, username=username)
-        comment = Moment_Person.objects.create(mid=moment, pid=person.pid, content=content, time=timezone.now())
+        comment = Moment_Person.objects.create(mid=moment, pid=person, content=content, time=timezone.now())
         return JsonResponse({'code': 0, 'message': '评论添加成功', 'data': {'id': comment.id}})
     else:
         return JsonResponse({'code': 405, 'message': '请求方法不允许'}, status=405)
@@ -96,7 +102,7 @@ def delete_comment(request):
         comment = get_object_or_404(Moment_Person, id=id, content__isnull=False)
         username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
         person = get_object_or_404(Person, username=username)
-        if comment.pid == person.pid:
+        if comment.pid.pid == person.pid:
             comment.delete()
             return JsonResponse({'code': 0, 'message': '评论已删除'})
         else:
@@ -107,20 +113,30 @@ def delete_comment(request):
 # @login_required
 def add_moment(request):
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            content = data.get('content')
-            tid = data.get('tid')
-            from person.models import Person
-            username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
-            person = get_object_or_404(Person, username=username)
-            aid = data.get('aid') if data.get('aid') else person.default_aid
-        except json.JSONDecodeError:
-            return JsonResponse({'code': 400, 'message': '请求体不是有效的 JSON 字符串'}, status=400)
-
+        content = request.POST.get('content')
+        tid = request.POST.get('tid')
+        
+        from person.models import Person
         username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
         person = get_object_or_404(Person, username=username)
-        moment = Moment.objects.create(creator=person.pid, text=content, tid_id=tid, aid_id=aid, time=timezone.now())
+        
+        aid = request.POST.get('aid') if request.POST.get('aid') else person.default_aid
+        aid = get_object_or_404(Album, aid=aid)
+
+        if tid:
+            tid = get_object_or_404(Trip, tid=tid)
+            moment = Moment.objects.create(creator=person, tid=tid, aid=aid, time=timezone.now(), content=content)
+        else:
+            moment = Moment.objects.create(creator=person, aid=aid, time=timezone.now(), content=content)
+        
+        images = request.FILES.getlist('images')
+        for image in images:
+            picture_name = FileSystemStorage(location=os.path.join(media_root, username, 'album/', aid.name)).save(image.name, image)
+            picture_url = base_url + settings.MEDIA_URL + username + '/album/' + aid.name + '/' + picture_name
+            picture = Picture.objects.create(creator=person, url=picture_url, file_name=picture_name, create_time=timezone.now())
+            picture_moment = Picture_Moment.objects.create(pid=picture, mid=moment)
+            picture_album = Picture_Album.objects.create(pid=picture, aid=aid)
+        
         return JsonResponse({'code': 0, 'message': '动态发表成功', 'data': {'mid': moment.mid}})
     else:
         return JsonResponse({'code': 405, 'message': '请求方法不允许'}, status=405)
@@ -137,7 +153,7 @@ def delete_moment(request):
         moment = get_object_or_404(Moment, mid=mid)
         username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
         person = get_object_or_404(Person, username=username)
-        if moment.creator == person.pid:
+        if moment.creator.pid == person.pid:
             moment.delete()
             return JsonResponse({'code': 0, 'message': '动态已删除'})
         else:
@@ -150,9 +166,27 @@ def get_moments(request):
     if request.method == 'POST':
         username = jwt.decode(request.headers['Authorization'].split(' ')[1], SECRET_KEY, algorithms=['HS256'])['username']
         person = get_object_or_404(Person, username=username)
-        # moments = Moment.objects.filter(creator=person.pid).values('mid', 'text', 'time', 'tid', 'aid')
-        moments = Moment.objects.filter(creator=person.pid).values('mid', 'text', 'time', 'tid')
-        return JsonResponse({'code': 0, 'message': '获取成功', 'data': {'moments': list(moments)}})
+        moments = Moment.objects.filter(creator=person)
+        # images = Picture_Moment.objects.filter(mid__in=[moment for moment in moments]).values('pid')
+        # images = Picture.objects.filter(pid__in=[image for image in images]).values('url')
+        moment_list = []
+        for moment in moments:
+            images = Picture_Moment.objects.filter(mid=moment.mid)
+            images = Picture.objects.filter(pid__in=images.values('pid'))
+            moment_list.append({
+                'mid': moment.mid,
+                'content': moment.content,
+                'createTime': moment.time.strftime('%Y-%m-%d %H:%M:%S'),
+                'tripId': moment.tid.tid if moment.tid else None,
+                'tripName': moment.tid.name if moment.tid else None,
+                'userId': moment.creator.pid,
+                'username': moment.creator.username,
+                'userAvatar': moment.creator.avatar_url,
+                'albumId': moment.aid.aid,
+                'albumName': moment.aid.name,
+                'images': [image.url for image in images]
+            })
+        return JsonResponse({'code': 0, 'message': '获取成功', 'data': {'moments': moment_list}})
     else:
         return JsonResponse({'code': 405, 'message': '请求方法不允许'}, status=405)
 
@@ -166,13 +200,20 @@ def get_moment_detail(request):
             return JsonResponse({'code': 400, 'message': '请求体不是有效的 JSON 字符串'}, status=400)
 
         moment = get_object_or_404(Moment, mid=mid)
+        images = Picture_Moment.objects.filter(mid=moment.mid)
+        images = Picture.objects.filter(pid__in=images.values('pid'))
         moment_data = {
             'mid': moment.mid,
-            'text': moment.text,
-            'creator': moment.creator.username,
-            'time': moment.time.isoformat(),
-            'tid': moment.tid.tid if moment.tid else None,
-            'aid': moment.aid.aid if moment.aid else None,
+            'content': moment.content,
+            'createTime': moment.time.strftime('%Y-%m-%d %H:%M:%S'),
+            'tripId': moment.tid.tid if moment.tid else None,
+            'tripName': moment.tid.name if moment.tid else None,
+            'userId': moment.creator.pid,
+            'username': moment.creator.username,
+            'userAvatar': moment.creator.avatar_url,
+            'albumId': moment.aid.aid,
+            'albumName': moment.aid.name,
+            'images': [image.url for image in images]
         }
         return JsonResponse({'code': 0, 'message': '获取成功', 'data': moment_data})
     else:
